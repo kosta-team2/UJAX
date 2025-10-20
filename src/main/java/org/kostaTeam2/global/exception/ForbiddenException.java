62e03d925a7c831bd2dffc7e70356bf757394759
MINYONG PARK <parkminyong22@gmail.com>
2025-10-20 15:02:50 +0900

    유저 인증관련 Exception 추가
    401 로그인 아닐때
    403 권한이 없을때

---
+++ src/main/java/org/kostaTeam2/global/exception/ForbiddenException.java
@@ -0,0 +1,14 @@
+package org.kostaTeam2.global.exception;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.kostaTeam2.global.exception.common.AppException;
+
+/**
+ * 인증은 되었지만 권한이 없는 사용자가 접근할 때 발생하는 예외.
+ * (예: 일반 사용자가 관리자 전용 기능 수행 시)
+ */
+public class ForbiddenException extends AppException {
+    public ForbiddenException(String message) {
+        super(HttpServletResponse.SC_FORBIDDEN, message);
+    }
+}
