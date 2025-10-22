8700024a5305d628540049370fae3c2ff0656f9f
MINYONG PARK <parkminyong22@gmail.com>
2025-10-23 00:50:22 +0900

    메서드네임수정

--- src/main/java/org/kostaTeam2/application/service/workspace/NoticeServiceImpl.java
+++ src/main/java/org/kostaTeam2/application/service/workspace/NoticeServiceImpl.java
@@ -1,5 +1,11 @@
 package org.kostaTeam2.application.service.workspace;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.kostaTeam2.domain.workspace.WorkspaceMember;
 import org.kostaTeam2.domain.workspace.WorkspaceMemberRepository;
 import org.kostaTeam2.domain.workspace.notice.Notice;
@@ -11,70 +17,67 @@ import org.kostaTeam2.global.exception.DBException;
 import org.kostaTeam2.global.exception.ForbiddenException;
 import org.kostaTeam2.global.exception.NotFoundException;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
 public class NoticeServiceImpl implements NoticeService {
-    private final DataSource ds;
-    private final NoticeRepository noticeRepository;
-    private final WorkspaceMemberRepository workspaceMemberRepository;
+	private final DataSource ds;
+	private final NoticeRepository noticeRepository;
+	private final WorkspaceMemberRepository workspaceMemberRepository;
 
-    public NoticeServiceImpl(DataSource ds, NoticeRepository noticeRepository, WorkspaceMemberRepository workspaceMemberRepository) {
-        this.ds = ds;
-        this.noticeRepository = noticeRepository;
-        this.workspaceMemberRepository = workspaceMemberRepository;
-    }
+	public NoticeServiceImpl(DataSource ds, NoticeRepository noticeRepository,
+		WorkspaceMemberRepository workspaceMemberRepository) {
+		this.ds = ds;
+		this.noticeRepository = noticeRepository;
+		this.workspaceMemberRepository = workspaceMemberRepository;
+	}
 
-    @Override
-    public void create(NoticeRequest dto) {
-        try (Connection conn = ds.getConnection()) {
-            WorkspaceMember workspaceMember = new WorkspaceMember(dto.userId(), dto.workspaceId());
-            // todo isLeader 서비스 이동
-//            if (!workspaceMemberRepository.isLeader(conn, workspaceMember)) {
-//                throw new ForbiddenException("리더가 아닙니다.");
-//            }
+	@Override
+	public void create(NoticeRequest dto) {
+		try (Connection conn = ds.getConnection()) {
+			WorkspaceMember workspaceMember = new WorkspaceMember(dto.userId(), dto.workspaceId());
+			// todo isLeader 서비스 이동
+			if (!workspaceMemberRepository.isLeader(conn, workspaceMember)) {
+				throw new ForbiddenException("리더가 아닙니다.");
+			}
 
-            Notice notice = Notice
-                    .create(
-                            dto.workspaceId(),
-                            new NoticeTitle(dto.title()),
-                            new NoticeContent(dto.content())
-                    );
+			Notice notice = Notice
+				.create(
+					dto.workspaceId(),
+					new NoticeTitle(dto.title()),
+					new NoticeContent(dto.content())
+				);
 
-            noticeRepository.save(conn, notice);
-        } catch (SQLException e) {
-            throw new DBException("공지 생성 실패");
-        }
-    }
+			noticeRepository.save(conn, notice);
+		} catch (SQLException e) {
+			throw new DBException("공지 생성 실패");
+		}
+	}
 
-    @Override
-    public void delete(NoticeRequest dto) {
-        try (Connection conn = ds.getConnection()) {
-            WorkspaceMember workspaceMember = new WorkspaceMember(dto.userId(), dto.workspaceId());
-//            if (!workspaceMemberRepository.isLeader(conn, workspaceMember)) {
-//                throw new ForbiddenException("리더가 아닙니다.");
-//            }
+	@Override
+	public void delete(NoticeRequest dto) {
+		try (Connection conn = ds.getConnection()) {
+			WorkspaceMember workspaceMember = new WorkspaceMember(dto.userId(), dto.workspaceId());
+			if (!workspaceMemberRepository.isLeader(conn, workspaceMember)) {
+				throw new ForbiddenException("리더가 아닙니다.");
+			}
 
-            if (noticeRepository.delete(conn, dto.noticeId()) == 0) throw new NotFoundException("삭제할 공지가 없습니다.");
-        } catch (SQLException e) {
-            throw new DBException("공지 삭제 실패");
-        }
-    }
+			if (noticeRepository.delete(conn, dto.noticeId()) == 0)
+				throw new NotFoundException("삭제할 공지가 없습니다.");
+		} catch (SQLException e) {
+			throw new DBException("공지 삭제 실패");
+		}
+	}
 
-    @Override
-    public List<Notice> getPageNotices(NoticeRequest dto) {
-        Long workspaceId = dto.workspaceId();
-//        String sort = dto.sort(); todo 정렬기능 미구현
-        int page = dto.page();
-        int limit = dto.limit();
-        int offset = page*limit;
+	@Override
+	public List<Notice> getPageNotices(NoticeRequest dto) {
+		Long workspaceId = dto.workspaceId();
+		//        String sort = dto.sort(); todo 정렬기능 미구현
+		int page = dto.page();
+		int limit = dto.limit();
+		int offset = page * limit;
 
-        try (Connection conn = ds.getConnection()) {
-            return noticeRepository.findNoticePageByWorkspaceId(conn, dto.workspaceId(), offset, limit);
-        } catch (SQLException e) {
-            throw new DBException("공지 불러오기 실패");
-        }
-    }
+		try (Connection conn = ds.getConnection()) {
+			return noticeRepository.findNoticePageByWorkspaceId(conn, dto.workspaceId(), offset, limit);
+		} catch (SQLException e) {
+			throw new DBException("공지 불러오기 실패");
+		}
+	}
 }
--- src/main/java/org/kostaTeam2/presentation/controller/page/NoticePageController.java
+++ src/main/java/org/kostaTeam2/presentation/controller/page/NoticePageController.java
@@ -1,7 +1,7 @@
 package org.kostaTeam2.presentation.controller.page;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+
 import org.kostaTeam2.application.service.workspace.NoticeService;
 import org.kostaTeam2.domain.workspace.notice.Notice;
 import org.kostaTeam2.dto.request.NoticeRequest;
@@ -9,79 +9,80 @@ import org.kostaTeam2.global.exception.BadRequestException;
 import org.kostaTeam2.presentation.controller.dto.SessionUser;
 import org.kostaTeam2.presentation.view.ModelAndView;
 
-import java.util.List;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class NoticePageController implements Controller {
-    private final NoticeService noticeService;
+	private final NoticeService noticeService;
 
-    public NoticePageController(NoticeService noticeService) {
-        this.noticeService = noticeService;
-    }
+	public NoticePageController(NoticeService noticeService) {
+		this.noticeService = noticeService;
+	}
 
-    @Override
-    public ModelAndView handle(String methodName, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return switch (methodName) {
-            case "createNotice" -> create(request, response);
-            case "getNotice" -> getList(request, response);
-            case "deleteNotice" -> delete(request, response);
-            default -> throw new BadRequestException("workspace methodName이 올바르지 않습니다.");
-        };
-    }
+	@Override
+	public ModelAndView handle(String methodName, HttpServletRequest request, HttpServletResponse response) throws
+		Exception {
+		return switch (methodName) {
+			case "createNotice" -> create(request, response);
+			case "getNotices" -> getList(request, response);
+			case "deleteNotice" -> delete(request, response);
+			default -> throw new BadRequestException("workspace methodName이 올바르지 않습니다.");
+		};
+	}
 
-    private ModelAndView create(HttpServletRequest request, HttpServletResponse response) {
-        SessionUser sessionUser = (SessionUser) request.getSession().getAttribute("SessionUser");
-        Long workspaceId = Long.valueOf(request.getParameter("workspaceId"));
-        String title = request.getParameter("noticeTitle");
-        String content = request.getParameter("noticeContent");
+	private ModelAndView create(HttpServletRequest request, HttpServletResponse response) {
+		SessionUser sessionUser = (SessionUser)request.getSession().getAttribute("SessionUser");
+		Long workspaceId = Long.valueOf(request.getParameter("workspaceId"));
+		String title = request.getParameter("noticeTitle");
+		String content = request.getParameter("noticeContent");
 
-        noticeService.create(NoticeRequest
-                .fromCreate(
-                        sessionUser.memberId(),
-                        workspaceId,
-                        title,
-                        content
-                ));
+		noticeService.create(NoticeRequest
+			.fromCreate(
+				sessionUser.memberId(),
+				workspaceId,
+				title,
+				content
+			));
 
-//        request.setAttribute("wsId", workspaceId);
-        // todo 과연 어떤 워크스페이스의 notice.jsp로?
-        String target = request.getContextPath() + "/workspace/";
-        return new ModelAndView(target, true);
-    }
+		//        request.setAttribute("wsId", workspaceId);
+		// todo 과연 어떤 워크스페이스의 notice.jsp로?
+		String target = request.getContextPath() + "/workspace/";
+		return new ModelAndView(target, true);
+	}
 
-    private ModelAndView getList(HttpServletRequest request, HttpServletResponse response) {
-        Long workspaceId = Long.valueOf(request.getParameter("workspaceId"));
-        String sort =  request.getParameter("sort");
-        Integer page = Integer.valueOf(request.getParameter("page"));
-        Integer limit = Integer.valueOf(request.getParameter("limit"));
+	private ModelAndView getList(HttpServletRequest request, HttpServletResponse response) {
+		Long workspaceId = Long.valueOf(request.getParameter("workspaceId"));
+		String sort = request.getParameter("sort");
+		Integer page = Integer.valueOf(request.getParameter("page"));
+		Integer limit = Integer.valueOf(request.getParameter("limit"));
 
-        List<Notice> noticeList = noticeService.getPageNotices(NoticeRequest
-                .fromReadList(
-                        workspaceId,
-                        sort,
-                        page,
-                        limit
-                ));
+		List<Notice> noticeList = noticeService.getPageNotices(NoticeRequest
+			.fromReadList(
+				workspaceId,
+				sort,
+				page,
+				limit
+			));
 
-        request.setAttribute("noticeList", noticeList);
-        String target = request.getContextPath() + "/workspace/notice.jsp";
-        return new ModelAndView(target);
-    }
+		request.setAttribute("noticeList", noticeList);
+		String target = request.getContextPath() + "/workspace/notice.jsp";
+		return new ModelAndView(target);
+	}
 
+	private ModelAndView delete(HttpServletRequest request, HttpServletResponse response) {
+		SessionUser sessionUser = (SessionUser)request.getSession().getAttribute("SessionUser");
+		Long workspaceId = Long.valueOf(request.getParameter("workspaceId"));
+		Long noticeId = Long.valueOf(request.getParameter("noticeId"));
 
-    private ModelAndView delete(HttpServletRequest request, HttpServletResponse response) {
-        SessionUser sessionUser = (SessionUser) request.getSession().getAttribute("SessionUser");
-        Long workspaceId = Long.valueOf(request.getParameter("workspaceId"));
-        Long noticeId = Long.valueOf(request.getParameter("noticeId"));
+		noticeService.delete(NoticeRequest
+			.fromDelete(
+				sessionUser.memberId(),
+				workspaceId,
+				noticeId
+			));
 
-        noticeService.delete(NoticeRequest
-                .fromDelete(
-                        sessionUser.memberId(),
-                        workspaceId,
-                        noticeId
-                ));
-
-        String target = request.getContextPath() + "/workspace/notice.jsp";
-        return new ModelAndView(target);
-    }
+		String target = request.getContextPath() + "/workspace/notice.jsp";
+		return new ModelAndView(target);
+	}
 
 }
