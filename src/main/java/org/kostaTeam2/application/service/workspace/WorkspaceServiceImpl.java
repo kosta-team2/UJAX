f98aca7bfd39577189ef54442c3d6c12a93b6bb4
jinukee <jinukee333@gmail.com>
2025-10-23 22:09:37 +0900

    feat : 리더가 워크스페이스 나가려는 경우 오류 메세지 ValidationException으로 메시지 전달

--- src/main/java/org/kostaTeam2/application/service/workspace/WorkspaceServiceImpl.java
+++ src/main/java/org/kostaTeam2/application/service/workspace/WorkspaceServiceImpl.java
@@ -10,6 +10,7 @@ import org.kostaTeam2.global.exception.BadRequestException;
 import org.kostaTeam2.global.exception.DBException;
 import org.kostaTeam2.global.exception.ForbiddenException;
 import org.kostaTeam2.global.exception.common.AppException;
+import org.kostaTeam2.global.exception.common.ValidationException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -196,7 +197,8 @@ public class WorkspaceServiceImpl implements WorkspaceService {
             if (workspaceMemberRepository.isLeader(con, new WorkspaceMember(wsId, whoAmI))) {
                 //워크스페이스에 남아 있는 사람이 1명인지 check
                 if (!workspaceMemberRepository.amIOnlyPerson(con, wsId)) {
-                    throw new BadRequestException("리더를 위임하고 탈퇴하세요.");
+                    throw new ValidationException("리더를 위임하고 탈퇴하세요.",
+                            "/front?key=workspace&methodName=show&workspaceId="+dto.wsId());
                 }
             }
 
