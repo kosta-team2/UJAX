7a067b59dd1b6d97a96d61e1de322903b5537ced
jinukee <jinukee333@gmail.com>
2025-10-28 13:57:12 +0900

    fix : logout 안 먹는거 해결

--- src/main/webapp/workspace/js/header.js
+++ src/main/webapp/workspace/js/header.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const logoutBtn = document.querySelector('.logout-btn');
-    const modeToggle = document.getElementById('modeToggle');
-    const themeLink = document.getElementById('theme-style');
+    // const modeToggle = document.getElementById('modeToggle');
+    // const themeLink = document.getElementById('theme-style');
 
-    // 다크모드 토글
-    modeToggle.addEventListener('click', () => {
-        const isLight = themeLink.getAttribute('href').includes('lightmode.css');
-
-        if (isLight) {
-            themeLink.href = '../common/css/darkmode.css';
-            modeToggle.textContent = '☀️ Light';
-        } else {
-            themeLink.href = '../common/css/lightmode.css';
-            modeToggle.textContent = '🌙 Dark';
-        }
-    });
+    // // 다크모드 토글
+    // modeToggle.addEventListener('click', () => {
+    //     const isLight = themeLink.getAttribute('href').includes('lightmode.css');
+    //
+    //     if (isLight) {
+    //         themeLink.href = '../common/css/darkmode.css';
+    //         modeToggle.textContent = '☀️ Light';
+    //     } else {
+    //         themeLink.href = '../common/css/lightmode.css';
+    //         modeToggle.textContent = '🌙 Dark';
+    //     }
+    // });
 
     // 로그아웃 버튼 클릭 시
     logoutBtn.addEventListener('click', (e) => {
